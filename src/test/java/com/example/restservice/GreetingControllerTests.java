6ad574d0b78dbe7ef1879dd4ff6c99e95698379e
CoffeerLatte <todty2312@gmail.com>
2024-02-27 16:31:21 +0900

    update

--- src/test/java/com/example/restservice/GreetingControllerTests.java
+++ src/test/java/com/example/restservice/GreetingControllerTests.java
@@ -37,14 +37,14 @@ public class GreetingControllerTests {
 	@Test
 	public void noParamGreetingShouldReturnDefaultMessage() throws Exception {
 
-		this.mockMvc.perform(get("/greeting")).andDo(print()).andExpect(status().isOk())
+		this.mockMvc.perform(get("/hello")).andDo(print()).andExpect(status().isOk())
 				.andExpect(jsonPath("$.content").value("Hello, World!"));
 	}
 
 	@Test
 	public void paramGreetingShouldReturnTailoredMessage() throws Exception {
 
-		this.mockMvc.perform(get("/greeting").param("name", "Spring Community"))
+		this.mockMvc.perform(get("/hello").param("name", "Spring Community"))
 				.andDo(print()).andExpect(status().isOk())
 				.andExpect(jsonPath("$.content").value("Hello, Spring Community!"));
 	}
